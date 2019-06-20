014e1d4dfb2a58eef5306bce4fcc9f62e1889ed1
Arnaud CHONE <arnaud_chone@hotmail.com>
2019-06-20 15:20:50 +0200

    Added READ and DEL methods

--- wsgi.py
+++ wsgi.py
@@ -13,7 +13,8 @@ ma = Marshmallow(app)
 
 
 from models import Product
-from schemas import products_schema
+from schemas import products_schema, product_schema
+
 
 @app.route('/hello')
 def hello():
@@ -23,3 +24,25 @@ def hello():
 def products():
     products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
     return products_schema.jsonify(products)
+
+@app.route('/products/<int:product_id>')
+def get_product(product_id):
+    product = db.session.query(Product).get(product_id)
+    return product_schema.jsonify(product)
+
+
+@app.route('/products/<int:product_id>', methods=['DELETE'])
+def del_product(product_id):
+    product = db.session.query(Product).get(product_id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return ('Deleted product', 204)
+    else:
+        return ('Product not found', 404)
+
+@app.route('/products', methods=['POST'])
+def create_product():
+    products = db.session.query(Product).all()
+    return products_schema.jsonify(products)
+
